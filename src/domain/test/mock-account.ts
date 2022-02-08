d2ae03fe0d19b5ef49fabb53304b02acbec44b6b
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-08 20:37:05 -0300

    refactor: add barrel index pattern

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,6 +1,6 @@
 import faker from '@faker-js/faker'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
-import { AccountModel } from '../models/account.model'
+import { AccountModel } from '@/domain/models'
 
 export const mockAuthentication = (): AuthenticationParams => ({ email: faker.internet.email(), password: faker.internet.password() })
 
