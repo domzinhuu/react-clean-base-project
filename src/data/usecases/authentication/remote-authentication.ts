d6ac697ff264d65aa117bb88e16c6fc857b330cb
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-08 14:29:04 -0300

    feat: ensure remote authentication calls httpPost client with correct url

---
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -0,0 +1,8 @@
+import { HttpPostClient } from 'data/protocols/http/http-post-client'
+
+export class RemoteAuthentication {
+  constructor (private readonly htpPostClient: HttpPostClient, private readonly url: string) { }
+  async auth (): Promise<void> {
+    await this.htpPostClient.post(this.url)
+  }
+}
