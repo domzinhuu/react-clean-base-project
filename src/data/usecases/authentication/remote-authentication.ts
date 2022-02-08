82adf9c10cb0deb2ad8b2fcbb4452ea4a81667fa
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-08 17:41:06 -0300

    refactor: change HttpPostClient Protocols params to post

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,6 +3,6 @@ import { HttpPostClient } from 'data/protocols/http/http-post-client'
 export class RemoteAuthentication {
   constructor (private readonly htpPostClient: HttpPostClient, private readonly url: string) { }
   async auth (): Promise<void> {
-    await this.htpPostClient.post(this.url)
+    await this.htpPostClient.post({ url: this.url })
   }
 }
