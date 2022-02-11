a32269d3669fd333701fafd9eeea5ea2d21c24da
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-11 16:59:22 -0300

    test: ensure login start with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -3,10 +3,12 @@ import { render, screen } from '@testing-library/react'
 import { LoginPage } from '..'
 
 describe('LoginPage', () => {
-  test('Deve iniciar a tela com o conteudo do error wrapper oculto ', () => {
+  test('Deve iniciar a tela com o estado inicial ', () => {
     render(<LoginPage />)
     const loadingStatus = screen.getByRole('error-wrapper')
-
     expect(loadingStatus.childElementCount).toBe(0)
+
+    const submitButton: HTMLButtonElement = screen.getByText('Entrar')
+    expect(submitButton.disabled).toBe(true)
   })
 })
