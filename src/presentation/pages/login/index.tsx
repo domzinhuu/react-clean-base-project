2b53a8f625fe8caf928e931d1a14b04bd195e47d
Maique Rosa <maique.rosa@bitz.com.br>
2022-02-11 16:58:18 -0300

    feat: ensure login start with submit button disabled

--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
             placeholder="Digite sua senha"
           />
 
-          <button className={Styles.submit} type="submit">
+          <button disabled className={Styles.submit} type="submit">
             Entrar
           </button>
           <span className={Styles.link}>Criar conta</span>
